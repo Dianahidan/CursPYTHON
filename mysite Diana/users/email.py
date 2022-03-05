e25a9a9cdcb5b6e0a33223ec52cb4c9e3bed904c
Dianahidan <93214080+Dianahidan@users.noreply.github.com>
2022-03-05 23:09:58 +0200

    Delete mysite Diana directory

--- mysite Diana/users/email.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import send_mail
-
-
-def email():
-    subject = 'Mail subject'
-    message = 'Mail body'
-    email_from = 'from@mysite.com'
-    recipient_list = ['dianasiminabarbu@gmail.com']
-    send_mail(subject, message, email_from, recipient_list)
